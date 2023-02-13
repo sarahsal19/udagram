6873f8cc81a77738f792d33022ae85f1f9b8c31e
sarah <Sarah13azx@gmail.com>
2023-02-13 10:12:56 +0300

    try adding files

---
+++ udagram/udagram-api/www/sequelize.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sequelize = void 0;
+const sequelize_typescript_1 = require("sequelize-typescript");
+exports.sequelize = new sequelize_typescript_1.Sequelize("postgres://postgres:Sarah13zzxxss@database-1.cr6aoymsqie2.us-east-1.rds.amazonaws.com:5432/postgres");
+//# sourceMappingURL=sequelize.js.map
